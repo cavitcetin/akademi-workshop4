5f99cbe037fd1abd749920c8f28b05f020307f3f
Cavit Çetin <cavitce7@gmail.com>
2024-04-29 14:01:47 +0300

    first commit

--- src/Inheritance/BaseClass.java
+++ src/Inheritance/BaseClass.java
@@ -35,7 +35,7 @@ public class BaseClass<T> {
         this.createdDate = createdDate;
     }
 
-    public LocalDate ggetUpdatedDate() {
+    public LocalDate getUpdatedDate() {
         return updatedDate;
     }
 
